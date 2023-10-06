0255733a9508517e86bf079f6d223d5a18ed301d
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-10-06 00:23:38 -0400

    Update ts-basics.ts

--- ts-basics/ts-basics.ts
+++ ts-basics/ts-basics.ts
@@ -67,6 +67,80 @@ interface StudentInterface {
 
 const student1: StudentInterface = {id: 1007, name: "Rich Halpern", school: "Engineering" }
 console.log(student1) // Output: { "id": 1007, "name": "Rich Halpern", "school": "Engineering" } 
-console.log(user.id) // Output: 75
 
-// Type Assertion 
+// Interface with Functions
+interface Financials {
+    (revenue: number, operating_cost: number): number
+}
+
+const profit: Financials = (revenue: number, operating_cost: number): number => revenue - operating_cost
+console.log(profit(25000, 22000)) // Output: 3000
+
+// classes
+class Patron {
+    private card_number: number
+    last_name: string
+    first_name: string
+
+    constructor(card_number: number, last_name: string, first_name: string){
+        this.card_number = card_number
+        this.last_name = last_name
+        this.first_name = first_name
+    }
+
+    confirm() {
+        return `The account by ${this.first_name} ${this.last_name} is active.`
+    }
+}
+
+const shwan = new Patron(5555, 'Michaels', 'Shwan')
+const bert = new Patron(9999, 'Harte', 'Bert')
+console.log(shwan) // Output: Patron: { "card_number": 5555, "last_name": "Michaels", :irst_name": "Shwan" } 
+console.log(bert) // Output: Patron: { "card_number": 9999, "last_name": "Harte", "first_name": "Bert" } 
+console.log(bert.confirm()) // Output: "The account by Bert Harte is active." 
+
+
+// Classes impleneting interfaces
+interface RegistrantInterface {
+    registration_number: number
+    name: string
+    register(): string
+}
+
+// Classes
+class Attendant implements RegistrantInterface {
+    registration_number: number
+    last_name: string
+    first_name: string
+
+    constructor(registration_number: number, last_name: string, first_name: string){
+        this.registration_number = registration_number
+        this.last_name = last_name
+        this.first_name = first_name
+    }
+
+    registration() {
+        return `The registrant ${this.first_name} ${this.last_name} is confirmed.`
+    }
+}
+
+const irwin = new Attendant(11114, 'Reese', 'Irwin')
+console.log(irwin) // Output: 11114, "last_name": "Reese", "first_name": "Irwin" } 
+console.log(irwin.registration()) // Output: "The registrant Irwin Reese is confirmed."
+
+// Subclasses
+class Reporter extends Attendant {
+    press_pass: number
+    reporter: string
+
+    constructor(press_pass: number, last_name: string, first_name: string, reporter: string) {
+        super(press_pass, last_name, first_name, reporter)
+        this.reporter = reporter
+        this.press_pass = press_pass
+    }
+}
+
+const harrison = new Reporter(56781, 'Harrison', 'John', 'Reporter')
+console.log(harrison.press_pass) // Output: 56781 
+console.log(harrison.reporter) // Output: "Reporter" 
+console.log(harrison.registration()) // Output: "The registrant John Harrison is confirmed." 
